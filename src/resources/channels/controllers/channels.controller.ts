036c829d8b211c5affb1d735b8e2792ac6a492dc
Alibek Dzhukaev <alibek.corvus@gmail.com>
2025-03-21 21:38:53 +0300

    feat: add transactions

--- src/resources/channels/controllers/channels.controller.ts
+++ src/resources/channels/controllers/channels.controller.ts
@@ -1,4 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { User } from '@app/resources/auth/decorators/user.decorator';
+import { JwtAuthGuard } from '@app/resources/auth/guards/jwt-auth.guard';
+import { AuthenticatedUser } from '@app/resources/auth/types/authenticatedUser';
+
+import { TransactionInterceptor } from '@common/interceptors/transaction.interceptor';
+import { Role } from '@common/roles/constants/roles.constant';
+import { Roles } from '@common/roles/decorators/roles.decorator';
+import { RolesGuard } from '@common/roles/guards/roles.guard';
 
 import { CreateChannelDto } from '../dto/create-channel.dto';
 import { UpdateChannelDto } from '../dto/update-channel.dto';
@@ -9,13 +28,16 @@ export class ChannelsController {
   constructor(private readonly channelsService: ChannelsService) {}
 
   @Post()
-  create(@Body() createChannelDto: CreateChannelDto) {
-    return this.channelsService.create(createChannelDto);
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN, Role.SUPER_ADMIN)
+  @UseInterceptors(TransactionInterceptor)
+  create(@Body() createChannelDto: CreateChannelDto, @User() user: AuthenticatedUser) {
+    return this.channelsService.create(createChannelDto, user.id);
   }
 
   @Get()
-  findAll() {
-    return this.channelsService.findAll();
+  findAll(@User() user: AuthenticatedUser) {
+    return this.channelsService.findAll(user.id);
   }
 
   @Get(':id')
@@ -24,11 +46,13 @@ export class ChannelsController {
   }
 
   @Patch(':id')
+  @UseInterceptors(TransactionInterceptor)
   update(@Param('id') id: string, @Body() updateChannelDto: UpdateChannelDto) {
     return this.channelsService.update(id, updateChannelDto);
   }
 
   @Delete(':id')
+  @UseInterceptors(TransactionInterceptor)
   remove(@Param('id') id: string) {
     return this.channelsService.remove(id);
   }
