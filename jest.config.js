3bb3f1eeb8b71407207bbb8ebf2feb9473b5cc4d
Alibek Dzhukaev <alibek.corvus@gmail.com>
2025-03-25 19:53:55 +0300

    feat: fixed jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,32 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
+/**
+ *
+ * @type {import('ts-jest')}
+ */
 module.exports = {
-    moduleFileExtensions: [
-        "js",
-        "json",
-        "ts"
-    ],
-    rootDir: "src",
-    testRegex: ".*\\.spec\\.ts$",
-    transform: {
-        "^.+\\.(t|j)s$": "ts-jest"
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  modulePaths: ['<rootDir>'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+      isolatedModules: true,
     },
-    collectCoverageFrom: [
-        "**/*.(t|j)s"
-    ],
-    coverageDirectory: "../coverage",
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+  },
+  roots: ['<rootDir>/src'],
+  verbose: true,
+  clearMocks: true,
 };
